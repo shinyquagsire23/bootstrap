36c0d79c7e1dae7a857f9c890f504b92f3f1ed73
gudenau <gudenaua@gmail.com>
2015-04-11 15:16:51 -0500

    Load payload from sd card
    I made a small change that loads the arm9 payload from the sd card. You
    need to name it payload.bin and put it in the same directory as the
    bootstrap.3dsx.

--- main.c
+++ main.c
@@ -1,10 +1,11 @@
 #include <3ds.h>
+#include <3ds/sdmc.h>
 #include <stdlib.h>
 #include <stdio.h>
 #include <string.h>
 #include <malloc.h>
 #include <dirent.h>
-#include "payload_bin.h"
+//#include "payload_bin.h"
 #include "bootstrap.h"
 
 void waitKey() {
@@ -29,25 +30,70 @@ void waitKey() {
 	}
 }
 
+void cleanup(void){
+	sdmcExit();
+	hbExit();
+	gfxExit();
+}
+
 int main()
 {
 	// Initialize services
 	gfxInitDefault(); // graphics
 	hbInit();
+	sdmcInit();
 
 	//consoleInit(GFX_TOP, NULL);
 
-	arm9_payload = payload_bin;
-	arm9_payload_size = payload_bin_size;
-
+	//arm9_payload = payload_bin;
+	//arm9_payload_size = payload_bin_size;
+	
+	// Open payload on SD card
+	FILE *payloadFile = fopen("payload.bin", "rb");
+	if(payloadFile == NULL){
+		printf("Error loading payload.bin\n");
+		waitKey();
+		cleanup();
+		return 0;
+	}
+	
+	// Get size of payload
+	fseek(payloadFile, 0, SEEK_END);
+	off_t size = ftell(payloadFile);
+	fseek(payloadFile, 0, SEEK_SET);
+	
+	// Allocate the payload buffer
+	printf("Allocating payload buffer...");
+	arm9_payload = malloc(size);
+	if(!arm9_payload){
+		printf("Could not allocate arm9 buffer, is the payload to large?");
+		waitKey();
+		cleanup();
+		return 0;
+	}
+	
+	// Read the payload
+	printf("Reading payload...");
+	off_t read = fread(arm9_payload, 1, size, payloadFile);
+	fclose(payloadFile);
+	
+	// Basic integrity check
+	if(size != read){
+		printf("Could not read payload.bin");
+		waitKey();
+		cleanup();
+		return 0;
+	}
+	
+	arm9_payload_size = size;
+	
     while (!doARM11Hax());
 
     printf("Success!\n\n");
 	waitKey();
 
 	// Exit services
-	hbExit();
-	gfxExit();
+	cleanup();
 	
 	// Return to hbmenu
 	return 0;
